7beb4dd170eecc41be6aa84d8da6768788ecaec5
Yusuke Tagawa <jeeproud@gmail.com>
2021-01-29 15:21:11 +0900

    refs: テストケースをスライスで表現、テストが通るように修正

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,5 +1,9 @@
 package main
 
+import "reflect"
+
 func walk(x interface{}, fn func(input string)) {
-	fn("I was planning that I have done this golang tutorial by the end of January, but it's remained much tasks")
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
 }
