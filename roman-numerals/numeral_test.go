4c4134ab02d6fa5a039dcfefcc77bfffb326bd5d
Yusuke Tagawa <jeeproud@gmail.com>
2021-01-31 21:29:00 +0900

    add: 2のときの処理を追加

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,10 +3,21 @@ package roman_numerals
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "Ⅰ"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to Ⅰ", 1, "Ⅰ"},
+		{"2 gets converted to Ⅱ", 2, "Ⅱ"},
+	}
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
 	}
 }
