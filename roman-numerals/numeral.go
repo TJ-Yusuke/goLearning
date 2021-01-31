379b03819a711b4e0fd2d0919c0ba7da4b358562
Yusuke Tagawa <jeeproud@gmail.com>
2021-01-31 22:09:16 +0900

    add: 50までの処理

--- roman-numerals/numeral.go
+++ roman-numerals/numeral.go
@@ -2,21 +2,29 @@ package roman_numerals
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{50, "L"},
+	{40, "XL"},
+	{10, "Ⅹ"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic > 4:
-			result.WriteString("Ⅴ")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("ⅠⅤ")
-			arabic -= 4
-		default:
-			result.WriteString("Ⅰ")
-			arabic--
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 
--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -8,12 +8,22 @@ func TestRomanNumerals(t *testing.T) {
 		Arabic      int
 		Want        string
 	}{
-		{"1 gets converted to Ⅰ", 1, "Ⅰ"},
-		{"2 gets converted to ⅠⅠ", 2, "ⅠⅠ"},
-		{"3 gets converted to ⅠⅠⅠ", 3, "ⅠⅠⅠ"},
-		{"4 gets converted to ⅠⅤ (can't repeat more than 3 times)", 4, "ⅠⅤ"},
-		{"5 gets converted to Ⅴ", 5, "Ⅴ"},
-		{"8 gets converted to ⅤⅠⅠⅠ", 8, "ⅤⅠⅠⅠ"},
+		{"1 gets converted to Ⅰ", 1, "I"},
+		{"2 gets converted to ⅠⅠ", 2, "II"},
+		{"3 gets converted to ⅠⅠⅠ", 3, "III"},
+		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"8 gets converted to VIII", 8, "VIII"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to Ⅹ", 10, "Ⅹ"},
+		{"14 gets converted to ⅩIV", 14, "ⅩIV"},
+		{"18 gets converted to ⅩVIII", 18, "ⅩVIII"},
+		{"20 gets converted to ⅩⅩ", 20, "ⅩⅩ"},
+		{"39 gets converted to ⅩⅩⅩIX", 39, "ⅩⅩⅩIX"},
+		{"40 gets converted to XL", 40, "XL"},
+		{"47 gets converted to XLVII", 47, "XLVII"},
+		{"49 gets converted to XLIX", 49, "XLIX"},
+		{"50 gets converted to L", 50, "L"},
 	}
 
 	for _, test := range cases {
