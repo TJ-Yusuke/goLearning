98d3d885c0fe09060d4ed933baf68ad51078bcd7
Yusuke Tagawa <jeeproud@gmail.com>
2021-01-31 22:20:24 +0900

    add: 10000未満までの処理

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -1,33 +1,46 @@
 package roman_numerals
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
+		Arabic int
+		Want   string
 	}{
-		{"1 gets converted to Ⅰ", 1, "I"},
-		{"2 gets converted to ⅠⅠ", 2, "II"},
-		{"3 gets converted to ⅠⅠⅠ", 3, "III"},
-		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
-		{"5 gets converted to V", 5, "V"},
-		{"8 gets converted to VIII", 8, "VIII"},
-		{"9 gets converted to IX", 9, "IX"},
-		{"10 gets converted to Ⅹ", 10, "Ⅹ"},
-		{"14 gets converted to ⅩIV", 14, "ⅩIV"},
-		{"18 gets converted to ⅩVIII", 18, "ⅩVIII"},
-		{"20 gets converted to ⅩⅩ", 20, "ⅩⅩ"},
-		{"39 gets converted to ⅩⅩⅩIX", 39, "ⅩⅩⅩIX"},
-		{"40 gets converted to XL", 40, "XL"},
-		{"47 gets converted to XLVII", 47, "XLVII"},
-		{"49 gets converted to XLIX", 49, "XLIX"},
-		{"50 gets converted to L", 50, "L"},
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{8, "VIII"},
+		{9, "IX"},
+		{10, "Ⅹ"},
+		{14, "ⅩIV"},
+		{18, "ⅩVIII"},
+		{20, "ⅩⅩ"},
+		{39, "ⅩⅩⅩIX"},
+		{40, "XL"},
+		{47, "XLVII"},
+		{49, "XLIX"},
+		{50, "L"},
+		{90, "XC"},
+		{100, "C"},
+		{400, "CD"},
+		{500, "D"},
+		{900, "CM"},
+		{1000, "M"},
+		{1984, "MCMLⅩⅩⅩIV"},
+		{3999, "MMMCMXCIX"},
+		{2014, "MMⅩIV"},
+		{1006, "MVI"},
+		{798, "DCCXCVIII"},
 	}
 
 	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Want), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
 			if got != test.Want {
 				t.Errorf("got %q, want %q", got, test.Want)
