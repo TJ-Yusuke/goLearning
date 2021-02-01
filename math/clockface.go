a0b02704d45809b961f1cfed7a005944f5d4330a
Yusuke Tagawa <jeeproud@gmail.com>
2021-02-01 14:50:44 +0900

    add: 秒針が30秒のときのテスト追加

--- math/clockface.go
+++ math/clockface.go
@@ -1,6 +1,9 @@
 package clockface
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 type Point struct {
 	X float64
@@ -10,4 +13,8 @@ type Point struct {
 //SecondHand is the unit vector of the second hand of an analogue clock at time 't' represented as a Point
 func SecondHand(t time.Time) Point {
 	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi
 }
--- math/clockface_test.go
+++ math/clockface_test.go
@@ -1,19 +1,18 @@
-package clockface_test
+package clockface
 
 import (
-	clockface "TJ-Yusuke/golearning/math"
+	"math"
 	"testing"
 	"time"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+func TestSecondsInRadians(t *testing.T) {
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
+	want := math.Pi
+	got := secondsInRadians(thirtySeconds)
 
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
+	if want != got {
+		t.Fatalf("Wanted %v radians, but got %v", want, got)
 	}
-
 }
