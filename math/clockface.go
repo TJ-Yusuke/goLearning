871e6a425cb1506765de1b3e182f394abaae9392
Yusuke Tagawa <jeeproud@gmail.com>
2021-02-02 21:45:00 +0900

    add: 分、秒数で時針の角度を更新

--- math/clockface.go
+++ math/clockface.go
@@ -27,7 +27,7 @@ func minuteHandPoint(t time.Time) Point {
 }
 
 func hoursInRadians(t time.Time) float64 {
-	return math.Pi / (6 / float64(t.Hour()%12))
+	return (minutesInRadians(t) / 12) + (math.Pi / (6 / float64(t.Hour()%12)))
 }
 
 func angleToPoint(angle float64) Point {
