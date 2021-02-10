0db8654778cf24bed683f8b67205d9769c9bd13a
Yusuke Tagawa <jeeproud@gmail.com>
2021-02-10 23:50:47 +0900

    add: content-typeヘッダーにapplication/jsonを指定 & テスト

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -10,6 +10,8 @@ import (
 	"testing"
 )
 
+const jsonContentType = "application/json"
+
 type StubPlayerStore struct {
 	scores   map[string]int
 	winCalls []string
@@ -125,6 +127,7 @@ func TestLeague(t *testing.T) {
 		got := getLeagueFromResponse(t, response.Body)
 		assertStatus(t, response.Code, http.StatusOK)
 		assertLeague(t, got, wantedLeague)
+		assertContentType(t, response, jsonContentType)
 	})
 }
 
@@ -174,4 +177,11 @@ func assertLeague(t *testing.T, got, want []Player) {
 func newLeagueRequest() *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, "/league", nil)
 	return req
+}
+
+func assertContentType(t *testing.T, response *httptest.ResponseRecorder, want string) {
+	t.Helper()
+	if response.Result().Header.Get("content-type") != want {
+		t.Errorf("response did not have content-type of %s, got %v", want, response.Result().Header)
+	}
 }
