22e57d522e0c60d90a3c67780104beaa6e75b534
Yusuke Tagawa <jeeproud@gmail.com>
2021-02-19 23:58:28 +0900

    サーバー実装

--- http-server/main.go
+++ http-server/main.go
@@ -3,12 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
-	server := NewPlayerServer(NewInmemoryPlayerStore())
+	//server := NewPlayerServer(NewInmemoryPlayerStore())
+	//
+	//if err := http.ListenAndServe(":5000", server); err != nil {
+	//	log.Fatalf("could not listen on port 5000 %v", err)
+	//}
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store := &FileSystemPlayerStore{db}
+	server := NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
+
 }
