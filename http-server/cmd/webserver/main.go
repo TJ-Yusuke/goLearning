cff6ac0e398608e67544016b3a86a2fae3fa827f
Yusuke Tagawa <jeeproud@gmail.com>
2021-02-23 15:03:01 +0900

    refs: ファイルを作成する関数作成

--- http-server/cmd/webserver/main.go
+++ http-server/cmd/webserver/main.go
@@ -4,23 +4,19 @@ import (
 	poker "TJ-Yusuke/golearning/http-server"
 	"log"
 	"net/http"
-	"os"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
 
-	store, err := poker.NewFileSystemPlayerStore(db)
+	defer close()
 
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v", err)
-	}
 	server := poker.NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
