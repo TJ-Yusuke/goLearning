f00a614bedd2f4914c2c9475862b61ff4e941b8d
Yusuke Tagawa <jeeproud@gmail.com>
2021-02-28 22:28:19 +0900

    add: アラート機能実装

--- http-server/CLI.go
+++ http-server/CLI.go
@@ -22,7 +22,7 @@ func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
 }
 
 func (cli *CLI) PlayPoker() {
-	cli.alerter.ScheduleAlertAt(5*time.Second, 100)
+	cli.scheduleBlindAlerts()
 	userInput := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(userInput))
 }
@@ -34,4 +34,13 @@ func extractWinner(userInput string) string {
 func (cli *CLI) readLine() string {
 	cli.in.Scan()
 	return cli.in.Text()
+}
+
+func (cli *CLI) scheduleBlindAlerts() {
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		cli.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + 10*time.Minute
+	}
 }
