fb9075ec78f3ba3bebca7e0979fadd6eb5cc0065
Yusuke Tagawa <jeeproud@gmail.com>
2021-03-04 12:15:41 +0900

    refs: 不要部分削除

--- http-server/CLI.go
+++ http-server/CLI.go
@@ -62,14 +62,3 @@ func (cli *CLI) readLine() string {
 	cli.in.Scan()
 	return cli.in.Text()
 }
-
-//func (cli *CLI) scheduleBlindAlerts(numberOfPlayers int) {
-//	blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
-//
-//	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
-//	blindTime := 0 * time.Second
-//	for _, blind := range blinds {
-//		cli.game.alerter.ScheduleAlertAt(blindTime, blind)
-//		blindTime = blindTime + blindIncrement
-//	}
-//}
