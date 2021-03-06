208790a1f7ff763d2518d79e12e301a81fd3d6d7
Yusuke Tagawa <jeeproud@gmail.com>
2021-03-06 23:55:17 +0900

    refs: ブラインドアラートを送信できる様にリファクタリング

--- http-server/CLI_test.go
+++ http-server/CLI_test.go
@@ -16,13 +16,15 @@ var dummyStdOut = &bytes.Buffer{}
 type GameSpy struct {
 	StartCalled      bool
 	StartCalledWith  int
+	BlindAlert       []byte
 	FinishedCalled   bool
 	FinishCalledWith string
 }
 
-func (g *GameSpy) Start(numberOfPlayers int) {
+func (g *GameSpy) Start(numberOfPlayers int, out io.Writer) {
 	g.StartCalled = true
 	g.StartCalledWith = numberOfPlayers
+	out.Write(g.BlindAlert)
 }
 
 func (g *GameSpy) Finish(winner string) {
